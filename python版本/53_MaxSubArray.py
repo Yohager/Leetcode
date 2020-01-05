62fc703c97888661369655ac84def3fa8b47ab03
Yohager <guoyuhang@std.uestc.edu.cn>
2020-01-05 16:55:04 +0800

    Update 53_MaxSubArray.py

--- python版本/53_MaxSubArray.py
+++ python版本/53_MaxSubArray.py
@@ -1,6 +1,34 @@
 class Solution:
     def maxSubArray(self, nums) -> int:
-        sum_num = sum(nums)
-        i = 0
-        while i<len(nums):
+        '''
+        暴力解法
+        if len(nums) == 1:
+            return nums[0]
+        max_num = [-100,0,0]
+        for i in range(len(nums)):
+            for j in range(i,len(nums)):
+                if sum(nums[i:j+1]) > max_num[0]:
+                    max_num[0] = sum(nums[i:j+1])
+                    max_num[1] = i
+                    max_num[2] = j
+        return max_num
+        '''
+        '''
+        优化前缀和
+        length = len(nums)
+        max_sum = nums[0]
+        min_sum = sum_num = 0
+        for i in range(length):
+            sum_num += nums[i]
+            max_sum = max(max_sum,sum_num - min_sum)
+            min_sum = min(min_sum,sum_num)
+        return max_sum
+        '''
+        
+
+
+
+if __name__ == "__main__":
+    test = Solution()
+    print(test.maxSubArray([1,2]))
             
