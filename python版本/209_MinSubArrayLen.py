e2f4e52c7e7f78af0ef0306ad5ff1495025c9dd2
Yohager <guoyuhang@std.uestc.edu.cn>
2020-01-06 19:30:34 +0800

    update two solutions

--- python版本/209_MinSubArrayLen.py
+++ python版本/209_MinSubArrayLen.py
@@ -1,2 +1,27 @@
 class Solution:
-    def minSubArrayLen(self, s: int, nums: List[int]) -> int:
+    def minSubArrayLen(self, s, nums) -> int:
+        if sum(nums) < s:
+            return 0
+        #min_len = len(nums)
+        start = 0
+        result = len(nums)
+        sum_nums = 0
+        for i in range(len(nums)):
+            sum_nums += nums[i]
+            while (sum_nums >= s):
+                result = min(result,i-start+1)
+                sum_nums -= nums[start]
+                start += 1
+        return result
+
+
+
+
+if __name__ == "__main__":
+    nums = [1,4,4]
+    s = 4
+    test = Solution()
+    print(test.minSubArrayLen(s,nums))
+
+
+ 
--- python版本/53_MaxSubArray.py
+++ python版本/53_MaxSubArray.py
@@ -24,11 +24,29 @@ class Solution:
             min_sum = min(min_sum,sum_num)
         return max_sum
         '''
-        
+        #尝试使用分治思想的方法
+        length = len(nums)
+        if length == 1:
+            return nums[0]
+        else:
+            max_left = self.maxSubArray(nums[:length // 2])
+            max_right = self.maxSubArray(nums[length // 2 : ])
+        #下面计算中间最大子序列
+        max_l = nums[length // 2 -1]
+        temp = 0
+        for i in range(length // 2 -1,-1,-1):
+            temp += nums[i]
+            max_l = max(temp,max_l)
+        max_r = nums[length // 2]
+        temp = 0
+        for j in range(length // 2, length):
+            temp += nums[j]
+            max_r = max(temp,max_r)
+        return max(max_left,max_right,max_l+max_r)
 
 
 
 if __name__ == "__main__":
     test = Solution()
-    print(test.maxSubArray([1,2]))
+    print(test.maxSubArray([-2,1,-3,4,-1,2,1,-5,4]))
             
